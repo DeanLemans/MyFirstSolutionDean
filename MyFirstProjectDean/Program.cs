5284dc0719b20c9c16f444636eab91fe9d04642d
Dean <111811264+DeanLemans@users.noreply.github.com>
2023-09-25 10:31:35 +0200

    cheese

--- MyFirstProjectDean/Program.cs
+++ MyFirstProjectDean/Program.cs
@@ -2,13 +2,24 @@
 {
     internal class Program
     {
-        #region warning
+        #region test
         //cheese
         //hamburger
         //*cheese cheese cheese
         #endregion
         static void Main(string[] args)
         {
+            #region Veriables
+            // string variable
+
+            string name; //declaration
+            name = "Dean";
+
+            string lastName;
+            lastName = "Lemans";
+
+            #endregion
+
             Console.WriteLine("Hello, World!");
         }
     }
