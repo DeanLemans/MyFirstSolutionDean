72d49851a750cdf6970da5c2f17c9fbc53a18cf0
Dean <111811264+DeanLemans@users.noreply.github.com>
2023-09-25 11:58:52 +0200

    console writeline

--- MyFirstProjectDean/Program.cs
+++ MyFirstProjectDean/Program.cs
@@ -3,31 +3,31 @@
     internal class Program
     {
         #region test
-        //cheese
-        //hamburger
-        //*cheese cheese cheese
+        /*cheese
+        hamburger
+        eggs
+        cheese cheese cheese*/
         #endregion
         static void Main(string[] args)
         {
-            #region Veriables
             // string variable!
             string name; //declaration
             name = "Dean"; //assigment
-            string lastName = "Lemans";
+            string lastName = "Lemans"; //DAfusion
+           
 
             char warning = '!'; //character variable
-
             int age = 16;   //intinger variable
-
             float pi = 3.14159265359f;  //float point variable
-
             //double simplisticPi = 3.141;  //double varialbe
-
             bool lovesDogs = true;  //boolean variable
 
-            #endregion
-
-            Console.WriteLine("Hello, World!");
+            Console.Write(name);
+            Console.WriteLine(lastName);
+            Console.WriteLine(age); 
+            Console.WriteLine(pi);
+            Console.WriteLine(warning);
+            Console.WriteLine(lovesDogs);
         }
     }
 }
