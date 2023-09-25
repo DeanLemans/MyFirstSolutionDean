59d487160a9e127d677b042ce34a5237a55019a7
Dean <111811264+DeanLemans@users.noreply.github.com>
2023-09-25 12:23:22 +0200

    age name thingy

--- MyFirstProjectDean/Program.cs
+++ MyFirstProjectDean/Program.cs
@@ -15,19 +15,41 @@
             name = "Dean"; //assigment
             string lastName = "Lemans"; //DAfusion
            
-
             char warning = '!'; //character variable
             int age = 16;   //intinger variable
             float pi = 3.14159265359f;  //float point variable
             //double simplisticPi = 3.141;  //double varialbe
             bool lovesDogs = true;  //boolean variable
+            string userInputName;
+            string userInputAge;
 
-            Console.Write(name);
-            Console.WriteLine(lastName);
-            Console.WriteLine(age); 
-            Console.WriteLine(pi);
-            Console.WriteLine(warning);
-            Console.WriteLine(lovesDogs);
+            //variable console write test
+            //Console.Write(name);
+            //Console.WriteLine(lastName);
+            //Console.WriteLine(age); 
+            //Console.WriteLine(pi);
+            //Console.WriteLine(warning);
+            //Console.WriteLine(lovesDogs);
+
+            //simple command
+            Console.WriteLine("Hello my name is " + name + lastName);
+            Console.WriteLine($"I am {age} years old");
+            Console.Write("Great wizzard in the sky, is it true that i love dogs? ");
+            Console.WriteLine("It is {0} young seeker of knowledge.", lovesDogs);
+            Console.WriteLine(); //whitespace
+
+            //user input
+            Console.Write("Enter name or shall kill you: ");
+            userInputName = Console.ReadLine();
+
+            Console.WriteLine($"Name of user: {name}");
+
+
+            Console.Write("Now enter your age or i shall kill you: ");
+            userInputAge = Console.ReadLine();
+
+            Console.WriteLine($"Age of user: {age}");
+                
         }
     }
 }
