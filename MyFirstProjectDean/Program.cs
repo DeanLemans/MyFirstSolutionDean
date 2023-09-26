c09bb28a8e99b1ce4090933db83c7f055aa4a3eb
Dean <111811264+DeanLemans@users.noreply.github.com>
2023-09-26 15:34:36 +0200

    upgrades people

--- MyFirstProjectDean/Program.cs
+++ MyFirstProjectDean/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace MyFirstProjectDean
+﻿using System.Runtime.InteropServices;
+
+namespace MyFirstProjectDean
 {
     internal class Program
     {
@@ -20,8 +22,17 @@
             float pi = 3.14159265359f;  //float point variable
             //double simplisticPi = 3.141;  //double varialbe
             bool lovesDogs = true;  //boolean variable
+
+            // user input variables
             string userInputName;
             string userInputAge;
+            string userInputPassword;
+            bool userInputAnswerDog = true;
+
+            // variable for expected values
+            string expectedPassword = "cheese";
+            string expectedName = "bob";
+            string expectedAnswerDog = "yes";
 
             //variable console write test
             //Console.Write(name);
@@ -39,17 +50,51 @@
             Console.WriteLine(); //whitespace
 
             //user input
-            Console.Write("Enter name or shall kill you: ");
+            //Console.Write("Enter name or shall kill you: ");
+            //userInputName = Console.ReadLine();
+
+            //Console.WriteLine($"Name of user: {name}");
+
+
+            //Console.Write("Now enter your age or i shall kill you: ");
+            //userInputAge = Console.ReadLine();
+
+            //Console.WriteLine($"Age of user: {age}");
+            //if (userInputAge < 15) ;
+            //    Console.WriteLine("What in the fuck are you doing here?");
+
+
+            Console.WriteLine("do you like dogs?");
+            if (userInputAnswerDog == expectedAnswerDog)
+            {
+                Console.WriteLine("It is a good thing that you like dogs");
+            }
+            else
+            {
+                Console.WriteLine("Kill yourself");
+            }
+
+            // small login system
+            // name, lastname and password based
+
+            Console.Write("Give me your fucking name: ");
             userInputName = Console.ReadLine();
 
-            Console.WriteLine($"Name of user: {name}");
+            Console.Write("Enter the fucking password: ");
+            userInputPassword = Console.ReadLine();
+
+            if (userInputName == expectedName && userInputPassword == expectedPassword)
+            {
+                Console.WriteLine("hello user, how are you doing");
+            }
+            else
+            {
+                Console.WriteLine("Kill yourself");
+            }
 
 
-            Console.Write("Now enter your age or i shall kill you: ");
-            userInputAge = Console.ReadLine();
+        
 
-            Console.WriteLine($"Age of user: {age}");
-                
         }
     }
 }
