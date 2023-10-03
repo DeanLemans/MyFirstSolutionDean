f7e5a40301d1b06dc8082a04990c33673427bed6
Dean <111811264+DeanLemans@users.noreply.github.com>
2023-10-03 11:34:36 +0200

    f

--- scripting 2 full assigments/Program.cs
+++ scripting 2 full assigments/Program.cs
@@ -14,10 +14,12 @@
         // variable for expected values
         string expectedPassword = "cheese";
         string expectedName = "bob";
+        
 
         // user input variables
         string userInputName;
         string userInputPassword;
+        
 
         //weird fucking amore fucking (not litteraly) assignment.
         Console.Write("is it true that when the moon hits your eyes that it's like a big pizza pie that's amore? ");
@@ -38,19 +40,50 @@
         // small login system
         // name, lastname and password based
 
-        Console.Write("Give me your fucking name: ");
-        userInputName = Console.ReadLine();
+        //Console.Write("Give me your fucking name: ");
+        //userInputName = Console.ReadLine();
 
-        Console.Write("Enter the fucking password: ");
-        userInputPassword = Console.ReadLine();
+        //Console.Write("Enter the fucking password: ");
+        //userInputPassword = Console.ReadLine();
 
-        if (userInputName == expectedName && userInputPassword == expectedPassword)
+        //if (userInputName == expectedName && userInputPassword == expectedPassword)
+        //{
+        //    Console.WriteLine("hello user, how are you doing");
+        //}
+        //else
+        //{
+        //    Console.WriteLine("Kill yourself");
+        //}
+
+
+
+        string userAge;
+        
+        int minimumAge = 2;
+
+
+
+        Console.Write("how old are you?");
+        userAge = Console.ReadLine();
+
+        int age  = int.Parse(userAge);
+
+
+        if (minimumAge > age)
         {
-            Console.WriteLine("hello user, how are you doing");
+            Console.WriteLine("minimum age not reached; i will now kill you");
         }
-        else
+        else if (minimumAge > age) 
         {
-            Console.WriteLine("Kill yourself");
+            Console.WriteLine("minimum rached");
         }
+
+
+
+
+
+
+
+
     }
 }
