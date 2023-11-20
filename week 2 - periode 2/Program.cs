089baa3bf74f2be6aae21ad43acd6b8cd9342f3a
Dean <111811264+DeanLemans@users.noreply.github.com>
2023-11-20 10:40:37 +0100

    .

---
+++ week 2 - periode 2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Xml.Linq;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+
+
+        Console.WriteLine("please enter input: ");
+
+        string no = Console.ReadLine();
+        int yes;
+
+
+        if (int.TryParse(no, out yes))
+        {
+            Console.WriteLine($"i like this: ");
+            Console.WriteLine($"{yes}");
+        }
+        else
+        {
+            Console.WriteLine($"fuck you {yes}, all my homies like integers");
+        }
+
+
+    }
+}
