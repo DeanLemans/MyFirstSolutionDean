4fc2ef72a0811bd92ef1d214a2228b91b6a14883
Dean <111811264+DeanLemans@users.noreply.github.com>
2024-01-15 14:18:56 +0100

    f

--- catcannon/obj/Debug/net8.0/catcannon.AssemblyInfo.cs
+++ catcannon/obj/Debug/net8.0/catcannon.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("catcannon")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4bd181a8a9cdba7eff5501ff19542675cb97d69c")]
 [assembly: System.Reflection.AssemblyProductAttribute("catcannon")]
 [assembly: System.Reflection.AssemblyTitleAttribute("catcannon")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
